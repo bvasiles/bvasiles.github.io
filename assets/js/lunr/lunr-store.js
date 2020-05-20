f40fb54a9791ada2414f03acf3faddafb495e95a
Bogdan Vasilescu <vasilescu@gmail.com>
2020-05-20 00:14:21 -0400

    Built from 06fd442

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "CHI'15 Press Release",
+        "excerpt":"Having higher gender and tenure diversity is associated with higher productivity in GitHub teams. A joint team of computer science researchers from University of California, Davis and Eindhoven University of Technology, in The Netherlands, have conducted a study of the effects of gender and tenure diversity on productivity and turnover...","categories": [],
+        "tags": [],
+        "url": "https://bvasiles.github.io/chi-press/",
+        "teaser": null
+      },{
+        "title": "Gender and tenure diversity in GitHub teams",
+        "excerpt":"Diversity in teams arises from any attribute that people use to differentiate themselves from others. The most obvious attributes are arguably demographic (age, gender, culture, ethnicity), but they can also be related to pretty much everything else (for example, role, tenure, expertise, or even personality). In general, diversity in teams...","categories": [],
+        "tags": [],
+        "url": "https://bvasiles.github.io/gender-tenure-diversity-github/",
+        "teaser": null
+      },{
+        "title": "Conference Monitor",
+        "excerpt":"Recently I started experimenting with Highcharts to visualize our conference metrics in the browser. Here’s a sample. Conference organizers may find these useful. Click (tap) on conference names in the legend below each plot to enable those series. The data is stored in this GitHub repo and loaded dynamically on...","categories": [],
+        "tags": [],
+        "url": "https://bvasiles.github.io/conference-monitor/",
+        "teaser": null
+      }]
